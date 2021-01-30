fe553b39d79c903fc9fcd8283986623aa8161185
unknown <Roy Mojica>
2021-01-30 17:32:38 -0600

    adding t-tests analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,3 +17,15 @@ suspensionData <- read.csv('Suspension_Coil.csv')
 total_summary <- suspensionData %>% summarize(psi_mean=mean(PSI), psi_median=median(PSI), psi_variance=var(PSI), psi_sd=sd(PSI))
 #Lot Summary
 lot_summary <- suspensionData %>% group_by(Manufacturing_Lot) %>% summarize(psi_mean=mean(PSI), psi_median=median(PSI), psi_variance=var(PSI), psi_sd=sd(PSI))
+
+?t.test()
+#T Test Analysis
+t.test(suspensionData$PSI, mu=1500)
+#lot 1 t Test
+t.test(subset(suspensionData, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+
+#lot 2 t Test
+t.test(subset(suspensionData, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+
+#lot 3 t Test
+t.test(subset(suspensionData, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
